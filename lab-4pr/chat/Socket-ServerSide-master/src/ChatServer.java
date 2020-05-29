39505336864a07c9c5cc1a1199aa9bea913931a2
rotciv998 <rotciv998@gmail.com>
2020-05-29 17:15:07 +0300

    init comit2

---
+++ lab-4pr/chat/Socket-ServerSide-master/src/ChatServer.java
@@ -0,0 +1,85 @@
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatServer {
+
+    private int port;
+    private Set<String> userNames = new HashSet<>();
+    private Set<UserThread> userThreads = new HashSet<>();
+
+    public ChatServer(int port) {
+        this.port = port;
+    }
+
+    public void execute() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            System.out.println("Chat Server is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New user connected");
+
+                UserThread newUser = new UserThread(socket, this);
+                userThreads.add(newUser);
+                newUser.start();
+
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error in the server: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Syntax: java ChatServer <port-number>");
+            System.exit(0);
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        ChatServer server = new ChatServer(port);
+        server.execute();
+    }
+
+  //Trimite mesajul de la un user la altii
+    void broadcast(String message, UserThread excludeUser) {
+        for (UserThread aUser : userThreads) {
+            if (aUser != excludeUser) {
+                aUser.sendMessage(message);
+            }
+        }
+    }
+
+
+    //adauga numele clientului nou conectat
+    void addUserName(String userName) {
+        userNames.add(userName);
+    }
+
+    //Cand clientul este deconectat ii sterge numele si thread-ul
+    void removeUser(String userName, UserThread aUser) {
+        boolean removed = userNames.remove(userName);
+        if (removed) {
+            userThreads.remove(aUser);
+            System.out.println("The user " + userName + " quitted");
+        }
+    }
+
+    Set<String> getUserNames() {
+        return this.userNames;
+    }
+
+    //Returneaza true daca alti clienti s-au conectat
+    boolean hasUsers() {
+        return !this.userNames.isEmpty();
+    }
+
+}
